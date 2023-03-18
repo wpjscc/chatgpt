ed07f2c6fd7421b0776047df210fab877aceac57
Your Name <you@example.com>
2023-03-19 00:42:38 +0800

    图片

--- assets/chatgpt.js
+++ assets/chatgpt.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     (function(){
         var start_x, start_y
-        var width = 400
+        var width = 720
         var height = 720
         var frame_height = 675
-        var chat_href = 'https://chatgpt.xiaofuwu.wpjs.cc/?is_hidden_left=1'
+        var chat_domain = 'https://chatgpt.xiaofuwu.wpjs.cc'
 
         var style = document.createElement('style');
         style.type = 'text/css';
@@ -13,21 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
             bottom: 120px;
             right: 10px;
             background-color: #fff;
-            border: 1px solid #ccc;
-            padding-left: 10px;
-            padding-right: 10px;
             box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
             z-index: 9999;
             cursor: pointer
-          }
-          .info-xxx-box:after {
-            content: "";
-            position: absolute;
-            top: -20px;
-            right: 10px;
-            border-width: 10px;
-            border-style: solid;
-            border-color: transparent transparent #fff transparent;
           }
           .popup-xxx-shadow {
             position: fixed;
@@ -51,14 +39,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const bottom_div = document.createElement('div')
         bottom_div.classList.add('info-xxx-box')
-        bottom_div.innerHTML = `Chatgpt`
+        bottom_div.innerHTML = `<img src="${chat_domain+'/assets/images/chatgpt.png'}" width="45" height="45">`
         document.body.appendChild(bottom_div);
 
         var id = randomString(10)
 
         bottom_div.addEventListener("click", function () {
             if (isMobile()) {
-                window.location.href = chat_href
+                window.location.href = chat_domain +'/?is_hidden_left=1'
             } else {
                 toggleElementVisibility(id)
                 toggleElementVisibility(shadow_id)
@@ -89,6 +77,8 @@ document.addEventListener('DOMContentLoaded', function() {
         h4.innerText = 'Chatgpt'
         h4.style.textAlign = 'center'
 
+        div.appendChild(h4)
+
         window.addEventListener('resize', function(){
             div.style.left = (window.innerWidth/2-width/2) + 'px'
             div.style.top = (window.innerHeight / 2 - height / 2) + 'px'
@@ -100,7 +90,7 @@ document.addEventListener('DOMContentLoaded', function() {
         iframe.height = frame_height; // iframe 的高度
         iframe.id = id + 'iframe';
         iframe.style.border = 'none'
-        iframe.src=chat_href
+        iframe.src=chat_domain+'/?is_hidden_left=1'
         div.appendChild(iframe)
         document.body.appendChild(div);
 
@@ -134,7 +124,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
         function mouseDown(e) {
             var div = document.getElementById(id);
-            div.style.transform = 'none'
 
             start_x = e.clientX - div.getBoundingClientRect().left
             start_y=e.clientY- div.getBoundingClientRect().top
